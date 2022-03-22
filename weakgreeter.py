56bc7f96edda721c3ec04af3ec070a5758f1e51c
tsayeau <49449835+tsayeau@users.noreply.github.com>
2022-03-22 18:08:57 -0400

    Added .stonk command
    .stonk ticker in chat to get a stock quote
    
    i.e.
    .stock TSLA
    
    weakgreeter: TSLA, Tesla, Inc. Current Price: 993.98 Daily Gain: 7.91 %

--- weakgreeter.py
+++ weakgreeter.py
@@ -1,8 +1,10 @@
 # weakgreeter is an IRC bot written in 2022, who knew IRC would still be in use?
 # weakgreet requires beautifulsoup (https://pypi.org/project/beautifulsoup4/) 
 # and requests (https://pypi.org/project/requests/)
+# and yfinance (https://pypi.org/project/yfinance/)
 
 # Import modules needed to weakgreeter
+from ast import Return
 import socket
 import time
 import config
@@ -10,10 +12,12 @@ import random
 import bs4
 import requests
 import re
+import yfinance as yf
 
 # pull the variables from the config file
 from config import *
 from bs4 import BeautifulSoup
+from numpy import empty
 
 # Connect to IRC server and authenticate with nickserv
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,6 +48,23 @@ def getUrl(string):
 def talk(msg):
     irc.send(bytes("PRIVMSG "+ channel +" :"+ msg +"\n", "UTF-8"))
 
+# Provide stock quotes to the IRC Channel    
+def stockMarket(quote):
+    stock = yf.Ticker(str(quote))
+    today = stock.history(period="1d")
+    currentPrice = today['Close']
+    if currentPrice.empty !=1:
+        today = stock.history(period="1d")
+        yesterday = stock.history(period="2d")
+        currentPrice = today['Close'][0]
+        openPrice = yesterday['Close'][0]
+        companyName = stock.info['shortName']
+        dailyGrowth = (float(currentPrice)-float(openPrice))/float(openPrice)*100
+        talk(quote + ", " + companyName + " " + "Current Price: " + str((round(float(currentPrice),2))) + " " + "Daily Gain: " + str((round(float(dailyGrowth),2)))+" %")
+    else:
+        talk("idk that stock u paper handed fool")
+    Return(quote)
+
 def main():
     joinChannel(channel)
     while 1:
@@ -62,6 +83,7 @@ def main():
                 talk("Hei trefirefem! Have you got your dad strength yet, or do you still not bench 2pl8?")
             elif message.find("Hi " + botNickName) != -1 or message.find("hi " + botNickName) != -1:
                 talk(random.choice(greetings) +" " + name)
+            
             # Look for links in chat and use BeautifulSoup to get the title back
             elif message.find("https://") != -1:
                 url = message = ircChat.split('PRIVMSG',1)[1].split(':',1)[1]
@@ -69,7 +91,12 @@ def main():
                 soup = BeautifulSoup(page.text, 'html.parser')
                 for title in soup.find_all('title'):
                     talk(name +"'s link is: " + title.get_text())
-        
+
+            # Look for stock quotes using the .stock command
+            elif message.find(".stock") != -1:
+                stonk = message = ircChat.split('PRIVMSG',1)[1].split('.stock',1)[1]
+                stockMarket(stonk.lstrip())
+
         # Respond to PINGs from the server as they appear        
         else:
             if ircChat.find("PING :") != -1:
