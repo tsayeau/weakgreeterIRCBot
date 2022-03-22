3ab5ed7197f465e0cdbb01924520a60357ee1761
tsayeau <49449835+tsayeau@users.noreply.github.com>
2022-03-21 21:53:55 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Bot configurations and passwords to be sent to weakgreeter.py
+server = "server" # IRC server address
+port = 6667 # port number your IRC server uses
+channel = "#channel" # Channel you wish to join
+botNickName = "weakgreeter" # Bot's nickname
+password = "password" # password you use to authenticate against the server's nickserv (optional but recommended)
+
+# Various greetings for weakgreeter to say
+greetings = ["Aloha", "Bonjour", "Hola", "Hi", "Konichiwa", "Hei", "Hello", "G'day", "Privet", "Ni Hao", "Guten tag"]
