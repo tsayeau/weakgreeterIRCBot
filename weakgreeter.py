0d843a55dba9b930058300fb4f9f02bf34fc525f
tsayeau <49449835+tsayeau@users.noreply.github.com>
2022-03-22 22:08:19 -0400

    ran through black
    ty bestcran

--- weakgreeter.py
+++ weakgreeter.py
@@ -1,5 +1,5 @@
 # weakgreeter is an IRC bot written in 2022, who knew IRC would still be in use?
-# weakgreet requires beautifulsoup (https://pypi.org/project/beautifulsoup4/) 
+# weakgreet requires beautifulsoup (https://pypi.org/project/beautifulsoup4/)
 # and requests (https://pypi.org/project/requests/)
 # and yfinance (https://pypi.org/project/yfinance/)
 
@@ -22,83 +22,126 @@ from numpy import empty
 # Connect to IRC server and authenticate with nickserv
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, port))
-irc.send(bytes("USER "+ botNickName +" "+ botNickName +" "+ botNickName + " " + "WeakGreeterIRCBOT" + "\n", "UTF-8"))
-irc.send(bytes("NICK "+ botNickName +"\n", "UTF-8"))
-irc.send(bytes('NICK ' + botNickName + '\r\n', "UTF-8"))
+irc.send(
+    bytes(
+        "USER "
+        + botNickName
+        + " "
+        + botNickName
+        + " "
+        + botNickName
+        + " "
+        + "WeakGreeterIRCBOT"
+        + "\n",
+        "UTF-8",
+    )
+)
+irc.send(bytes("NICK " + botNickName + "\n", "UTF-8"))
+irc.send(bytes("NICK " + botNickName + "\r\n", "UTF-8"))
 irc.send(bytes("NICKSERV IDENTIFY " + password + "\n", "UTF-8"))
 # Join channel
 def joinChannel(channel):
     ircChat = ""
-    irc.send(bytes("JOIN "+ channel +"\n", "UTF-8"))
+    irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
     ircChat = irc.recv(1024).decode("UTF-8")
-    ircChat = ircChat.strip('\n\r')
+    ircChat = ircChat.strip("\n\r")
     print(ircChat)
+
 
 # Respond to IRC server pings and keep bot alive
 def ping():
-  irc.send(bytes("PONG \n", "UTF-8"))
-  print("Pong sent!")
+    irc.send(bytes("PONG \n", "UTF-8"))
+    print("Pong sent!")
+
+
 # Identify URLs in the IRC chat stream
 def getUrl(string):
-    
+
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)      
+    url = re.findall(regex, string)
     return [x[0] for x in url]
+
+
 # Send messages to the IRC Channel
 def talk(msg):
-    irc.send(bytes("PRIVMSG "+ channel +" :"+ msg +"\n", "UTF-8"))
+    irc.send(bytes("PRIVMSG " + channel + " :" + msg + "\n", "UTF-8"))
 
-# Provide stock quotes to the IRC Channel    
+
+# Provide stock quotes to the IRC Channel
 def stockMarket(quote):
     stock = yf.Ticker(str(quote))
     today = stock.history(period="1d")
-    currentPrice = today['Close']
-    if currentPrice.empty !=1:
+    currentPrice = today["Close"]
+    if currentPrice.empty != 1:
         today = stock.history(period="1d")
         yesterday = stock.history(period="2d")
-        currentPrice = today['Close'][0]
-        openPrice = yesterday['Close'][0]
-        companyName = stock.info['shortName']
-        dailyGrowth = (float(currentPrice)-float(openPrice))/float(openPrice)*100
-        talk(quote + ", " + companyName + " " + "Current Price: " + str((round(float(currentPrice),2))) + " " + "Daily Gain: " + str((round(float(dailyGrowth),2)))+" %")
+        currentPrice = today["Close"][0]
+        openPrice = yesterday["Close"][0]
+        companyName = stock.info["shortName"]
+        dailyGrowth = (float(currentPrice) - float(openPrice)) / float(openPrice) * 100
+        talk(
+            quote
+            + ", "
+            + companyName
+            + " "
+            + "Current Price: "
+            + str((round(float(currentPrice), 2)))
+            + " "
+            + "Daily Gain: "
+            + str((round(float(dailyGrowth), 2)))
+            + " %"
+        )
     else:
         talk("idk that stock u paper handed fool")
     Return(quote)
 
+
 def main():
     joinChannel(channel)
     while 1:
-        ircChat= ""
+        ircChat = ""
         ircChat = irc.recv(1024).decode("UTF-8")
-        ircChat = ircChat.strip('\n\r')
+        ircChat = ircChat.strip("\n\r")
         print(ircChat)
 
         if ircChat.find("PRIVMSG") != -1:
             # Grab user's nick and split from the rest of the message
-            name = ircChat.split('!',1)[0][1:]
+            name = ircChat.split("!", 1)[0][1:]
             # Grab user's message and split from nick
-            message = ircChat.split('PRIVMSG',1)[1].split(':',1)[1]
+            message = ircChat.split("PRIVMSG", 1)[1].split(":", 1)[1]
             # If someone says hi don't be rude, say hi back!
-            if message.find("Hi " + botNickName) and name == "trefirefem" != -1 or message.find("hi " + botNickName) and name == "trefirefem"!= -1:
-                talk("Hei trefirefem! Have you got your dad strength yet, or do you still not bench 2pl8?")
-            elif message.find("Hi " + botNickName) != -1 or message.find("hi " + botNickName) != -1:
-                talk(random.choice(greetings) +" " + name)
-            
+            if (
+                message.find("Hi " + botNickName)
+                and name == "trefirefem" != -1
+                or message.find("hi " + botNickName)
+                and name == "trefirefem" != -1
+            ):
+                talk(
+                    "Hei trefirefem! Have you got your dad strength yet, or do you still not bench 2pl8?"
+                )
+            elif (
+                message.find("Hi " + botNickName) != -1
+                or message.find("hi " + botNickName) != -1
+            ):
+                talk(random.choice(greetings) + " " + name)
+
             # Look for links in chat and use BeautifulSoup to get the title back
             elif message.find("https://") != -1:
-                url = message = ircChat.split('PRIVMSG',1)[1].split(':',1)[1]
-                page = requests.get((','.join(getUrl(url))))
-                soup = BeautifulSoup(page.text, 'html.parser')
-                for title in soup.find_all('title'):
-                    talk(name +"'s link is: " + title.get_text())
+                url = message = ircChat.split("PRIVMSG", 1)[1].split(":", 1)[1]
+                page = requests.get((",".join(getUrl(url))))
+                soup = BeautifulSoup(page.text, "html.parser")
+                for title in soup.find_all("title"):
+                    talk(name + "'s link is: " + title.get_text())
 
             # Look for stock quotes using the .stock command
             elif message.find(".stock") != -1:
-                stonk = message = ircChat.split('PRIVMSG',1)[1].split('.stock',1)[1]
+                stonk = message = ircChat.split("PRIVMSG", 1)[1].split(".stock", 1)[1]
                 stockMarket(stonk.lstrip())
 
-        # Respond to PINGs from the server as they appear        
+        # Respond to PINGs from the server as they appear
         else:
             if ircChat.find("PING :") != -1:
-                ping()      
+                ping()
+
+
 main()
